54a2354bf83eeb7467f7d35d12469d5c9eaae3c0
Burak Tabanli <b.tabanli93@hotmail.com>
2025-03-23 15:53:25 +0100

    pom example

---
+++ src/test/java/page/TestApplicationPage.java
@@ -0,0 +1,24 @@
+//package page;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//import org.openqa.selenium.support.PageFactory;
+//import utils.Driver;
+//
+//
+//public class TestApplicationPage {
+//
+//    public TestApplicationPage() {
+//        PageFactory.initElements(Driver.getAndroidDriver(), this);
+//    }
+//
+//    @FindBy(xpath = "//android.view.ViewGroup[@content-desc=\"open menu\"]/android.widget.ImageView")
+//    public WebElement topLeftMenu;
+//
+//
+//
+//    //**************** Methods ****************
+//
+//    public void clickTopLeftMenu() {
+//        topLeftMenu.click();
+//    }
+//}
