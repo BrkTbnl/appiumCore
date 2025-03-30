ef4c624647ab57d3fbe9e7920fae1f5a1558d9b4
Burak Tabanli <b.tabanli93@hotmail.com>
2025-03-30 16:42:13 +0200

    util reusable methods scroll functionality added

--- src/test/java/utils/ReusableMethods.java
+++ src/test/java/utils/ReusableMethods.java
@@ -1,12 +1,12 @@
 package utils;
 
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static utils.Driver.getAndroidDriver;
 
@@ -27,6 +27,19 @@ public class ReusableMethods {
         tap.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), x, y));
         tap.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
         tap.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-        getAndroidDriver().perform(Arrays.asList(tap));
+        Hooks.androidDriver.perform(Arrays.asList(tap));
     }
+
+    public void scrollToFrom(int startPointX, int startPointY, int endPointX, int endPointY) {
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence swipe = new Sequence(finger, 0);
+
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), startPointX, startPointY));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), endPointX, endPointY));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        Hooks.androidDriver.perform(List.of(swipe));
+//        Hooks.androidDriver.perform(Collections.singletonList(swipe));
+    }
+
 }
